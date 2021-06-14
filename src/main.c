97052f6aa4bfd1ede94934968eae38433f779542
Rolf Madsen <rolfrm@gmail.com>
2021-06-14 23:22:26 +0200

    various cleanups

--- src/main.c
+++ src/main.c
@@ -15,49 +15,6 @@ const char * print_sqlite_code(int code){
   }
 }
 
-void sql_query(sqlite3 * db, const char * sql){
-  sqlite3_stmt * stmt = NULL;
-  int prep = sqlite3_prepare(db, sql, strlen(sql), &stmt, NULL);
-  for(int i = 0; i < 10; i++){
-    int ok = sqlite3_step(stmt);
-    int count = sqlite3_data_count(stmt);
-    for(int i = 0; i < count; i++)
-      logd("Value: %i\n", sqlite3_column_int(stmt, i));
-    if(ok == SQLITE_DONE){
-      break;
-    }
-  }
-  sqlite3_finalize(stmt);
-}
-
-int sql_query_int(sqlite3 * db, const char * sql){
-  sqlite3_stmt * stmt = NULL;
-  int prep = sqlite3_prepare(db, sql, strlen(sql), &stmt, NULL);
-  for(int i = 0; i < 10; i++){
-    int ok = sqlite3_step(stmt);
-    int count = sqlite3_data_count(stmt);
-    for(int i = 0; i < count; i++)
-      logd("Value: %i\n", sqlite3_column_int(stmt, i));
-    if(ok == SQLITE_DONE){
-      break;
-    }
-  }
-  sqlite3_finalize(stmt);
-  return 0;
-}
-
-void sql_exec_str(sqlite3 * db, const char * sql, const char * str){
-  sqlite3_stmt * stmt = NULL;
-  sqlite3_prepare(db, sql, strlen(sql), &stmt, NULL);
-  sqlite3_bind_text(stmt, 1, str, strlen(str) + 1, SQLITE_TRANSIENT);
-  for(int i = 0; i < 10; i++){
-    int ok = sqlite3_step(stmt);
-    if(ok == SQLITE_DONE)
-      break;
-    
-  }
-  sqlite3_finalize(stmt);
-}
 typedef enum {
 	      SQL_END = 0,
 	      SQL_INT,
@@ -65,12 +22,21 @@ typedef enum {
 	      SQL_STRING,
 }sql_value_type;
 
-void sql_exec(sqlite3 * db, const char * sql, ...){
-  sqlite3_stmt * stmt = NULL;
-  sqlite3_prepare(db, sql, strlen(sql), &stmt, NULL);
+void sql_exec_va(sqlite3_stmt ** cache, bool log, sqlite3 * db, const char * sql, va_list args){
+  sqlite3_stmt * stmt;
+  if(cache){
+    if(*cache == NULL){
+      sqlite3_prepare(db, sql, strlen(sql), &stmt, NULL);
+      *cache = stmt;
+    }else{
+      stmt = *cache;
+      sqlite3_clear_bindings(stmt);
+    }
+  }
+  else{
+    sqlite3_prepare(db, sql, strlen(sql), &stmt, NULL);
+  }
   int parmCount = sqlite3_bind_parameter_count(stmt);
-  va_list args;
-  va_start (args, sql);
   int i = 0;
   while(true){
     i += 1;
@@ -78,35 +44,60 @@ void sql_exec(sqlite3 * db, const char * sql, ...){
     switch(t){
     case SQL_END: goto end;
     case SQL_INT:
-      {
-	
+      {	
 	i64 v = va_arg(args, i64);
 	sqlite3_bind_int64(stmt, i, v);
 	break;
       }
     case SQL_DOUBLE:
       {
-	
-	i64 v = va_arg(args, f64);
+	f64 v = va_arg(args, f64);
 	sqlite3_bind_double(stmt, i, v);
 	break;
       }
     case SQL_STRING:
       {
 	char * v = va_arg(args, char *);
-	sqlite3_bind_text(stmt, i, v, strlen(v) + 1, SQLITE_TRANSIENT);
+	sqlite3_bind_text(stmt, i, v, strlen(v), SQLITE_TRANSIENT);
 	break;
       }
     }
   }
- end:
-  va_end(args);
+ end:;
 
-  for(int i = 0; i < 10; i++){
+  bool first_row = true;
+  while(true){
     int ok = sqlite3_step(stmt);
-    logd("E: %s\n", print_sqlite_code(ok));
     if(ok == SQLITE_DONE)
       break;
+    if(log && ok == SQLITE_ROW){
+      int count = sqlite3_data_count(stmt);
+      int column_count = sqlite3_column_count(stmt);
+      
+      if(first_row){
+	first_row = false;
+	for(int i = 0; i < column_count; i++){
+	  if(i > 0)
+	    logd(", ");
+	  logd("%s", sqlite3_column_name(stmt, i));
+	}
+	logd("\n");
+      }
+      for(int i = 0; i < count; i++){
+	if(i > 0)
+	  logd(", ");
+	var t = sqlite3_column_type(stmt, i);
+	
+	if(t == SQLITE_INTEGER)
+	  logd("%lld", sqlite3_column_int64(stmt, i));
+	if(t == SQLITE_TEXT)
+	  logd("%s", sqlite3_column_text(stmt, i));
+	if(t == SQLITE_FLOAT)
+	  logd("%f", sqlite3_column_double(stmt, i));
+      }
+      logd("\n");
+    }
+    
     if(ok == SQLITE_ERROR){
       const char * err = sqlite3_errmsg(db);
       ERROR("ERROR occured: %s", err);
@@ -118,14 +109,26 @@ void sql_exec(sqlite3 * db, const char * sql, ...){
       break;
     }
   }
-  sqlite3_finalize(stmt);
-
+  if(cache == NULL)
+    sqlite3_finalize(stmt);
 }
-  
-void test_io_methods(void){
-  sqlite3_io_methods io;
-  io.iVersion = 1;
-  
+
+void sql_exec(sqlite3 * db, const char * sql, ...){
+  va_list args;
+  va_start(args, sql);
+  sql_exec_va(NULL, false, db, sql, args);
+  va_end(args);
+}
+
+void sql_exec3(sqlite3 * db, bool log, const char * sql, ...){
+  va_list args;
+  va_start(args, sql);
+  sql_exec_va(NULL, log, db, sql, args);
+  va_end(args);
+}
+
+void sql_query(sqlite3 * db, const char * sql){
+  sql_exec(db, sql);
 }
 
 const char * mode_to_string(struct stat sb){
@@ -166,36 +169,10 @@ const char * dirent_to_type(int dirent_type){
   }
  }
 
+
 void print_files_table(sqlite3 * db){
-  logd("FILES\n");
   const char * sql = "SELECT * from files";
-  sqlite3_stmt * stmt = NULL;
-  int prep = sqlite3_prepare(db, sql, strlen(sql), &stmt, NULL);
-  while(true){
-    int ok = sqlite3_step(stmt);
-    int count = sqlite3_data_count(stmt);
-    logd("row: ");
-    for(int i = 0; i < count; i++){
-      if(i > 0)
-	logd(", ");
-      var t = sqlite3_column_type(stmt, i);
-      if(t == SQLITE_INTEGER)
-	logd("%i", sqlite3_column_int(stmt, i));
-      if(t == SQLITE_TEXT)
-	logd("%s", sqlite3_column_text(stmt, i));
-      
-    }
-    
-    logd("\n");
-    if(ok == SQLITE_DONE)
-      break;
-    if(ok == SQLITE_ERROR){
-      logd("ERROR\n");
-      break;
-    }
-  }
-  sqlite3_finalize(stmt);
-  
+  sql_exec3(db, true, sql);
 }
 
 void update_run(sqlite3 *db, const char * loc_path){
@@ -235,9 +212,22 @@ int main(int argc, char ** argv){
   sqlite3_open(":memory:", &db);
   sql_query(db, "pragma user_version = 150");
   sql_query(db, "pragma user_version");
-  sql_query(db, "create table files (name TEXT PRIMARY KEY, type INTEGER, size INTEGER, modified INTEGER, created INTEGER, permissions INTEGER);");
+  sql_query(db, "create table files (name TEXT PRIMARY KEY, type INTEGER, size BIGINTEGER, modified INTEGER, created INTEGER, permissions INTEGER);");
+
+  // temp_files should contain the current state of the calculation
+  // in addition to the fact if a thing has been processed to completion
+  // the minimal amount of processing differs for different types of objects
+  // for extremely large collections of files, the update operation may take a significant amount of time, therefore, it should be done in some chunks locally. It may be best if this part of the operation is contained in a separate macine-local file.
+  // for this purpose, the ATTACH statement might be the right way to make joins from a local DB to the backed DB.
+  sql_query(db, "create temp table temp_files (name TEXT PRIMARY KEY, type INTEGER, size BIGINTEGER, modified INTEGER, created INTEGER, permissions INTEGER, touch INTEGER);");
+
+  sql_query(db, "create temp table zero (touch INTEGER)");
+  sql_query(db, "INSERT INTO zero (touch) VALUES (0)");
+  
   update_run(db, "./iron");
-  sql_query_int(db, "SELECT count(name) from files");
+  sql_query(db, "INSERT INTO temp_files SELECT * FROM files JOIN zero");
+  sql_exec3(db, true, "SELECT count(name), sum(size)/1000000 from temp_files");
+  //sql_exec3(db, true, "SELECT * FROM temp_files");
   print_files_table(db);
   return 0;
   
